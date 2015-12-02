8e591693b73828364cf9b1dcd26f46b1a042dac2
Dan Printzell <xwildn00bx@gmail.com>
2015-12-02 13:58:53 +0100

    Added part2 to day1
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- day1/source/app.d
+++ day1/source/app.d
@@ -1,13 +1,21 @@
 import std.stdio;
 
 int main(string[] args) {
+	if (args.length != 2) {
+		writeln("Usage: ", args[0], " <DATA>");
+		return -1;
+	}
 	int level = 0;
-	foreach (arg; args)
-		foreach (ch; arg)
-			if (ch == '(')
-				level++;
-			else if (ch == ')')
-				level--;
+	ulong firstEnteredBasement = 0;
+	foreach (idx, ch; args[1]) {
+		if (ch == '(')
+			level++;
+		else if (ch == ')')
+			level--;
+		if (!firstEnteredBasement && level < 0)
+			firstEnteredBasement = idx + 1;
+	}
 	writeln("Result: ", level);
+	writeln("First entered basement: ", firstEnteredBasement);
 	return 0;
 }
