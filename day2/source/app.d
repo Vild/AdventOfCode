3b7ef9c74e8c48a378f57c89dbb6090d1ab0a57d
Dan Printzell <xwildn00bx@gmail.com>
2015-12-03 19:01:21 +0100

    Added day2 part2
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- day2/source/app.d
+++ day2/source/app.d
@@ -1,19 +1,30 @@
 import std.stdio;
 import std.algorithm;
 import std.range;
-import std.algorithm.iteration;
 import std.conv;
+
 int main(string[] args) {
-	 stdin
-	 	.byLine
+	//dfmt off
+	auto data = stdin
+		.byLine
 		.map!(line => line.split("x"))
 		.filter!(arr => arr.length > 1)
 		.map!(to!(int[]))
-		.map!(sort)
+		.map!(sort).array;
+
+	data
 		.map!(v =>
 			2*v[0]*v[1] + 2*v[1]*v[2] + 2*v[2]*v[0] + v[0] * v[1]
 		)
 		.sum
-		.writeln;
+		.writeln("ft² of papper is needed!");
+	data
+		.map!(v =>
+			2*v[0] + 2*v[1] +
+			v[0]*v[1]*v[2]
+		)
+		.sum
+		.writeln("ft of ribbon is needed!");
+	//dfmt on
 	return 0;
 }
