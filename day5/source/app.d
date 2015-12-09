480d72d5f7f7502df582eede25f0ba1d0450e2dc
Dan Printzell <xwildn00bx@gmail.com>
2015-12-09 23:03:55 +0100

    Added day5 part 2
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- day5/source/app.d
+++ day5/source/app.d
@@ -1,20 +1,32 @@
 import std.stdio;
 import std.algorithm;
+import std.regex;
+import std.conv;
 
 int main(string[] args) {
-	int count = 0;
-	foreach (x; stdin
-		.byLine
-		.map!(isNice))
-		if (x)
-			count++;
+	assert(args.length == 2, "USAGE: day5 <Part>");
+	ulong count = 0;
+	ulong part = args[1].to!ulong;
 
+	if (part == 1) {
+		foreach (x; stdin
+			.byLine
+			.map!(isNiceFirst))
+			if (x)
+				count++;
+	} else if (part == 2) {
+		foreach (x; stdin
+			.byLine
+			.map!(isNiceSecond))
+			if (x)
+				count++;
+	}
 	count.writeln(" strings are nice");
 
 	return 0;
 }
 
-bool isNice(inout(char[]) line) {
+bool isNiceFirst(inout(char[]) line) {
 	immutable VOWELS = "aeiou";
 
 	int vowels = 0;
@@ -39,9 +51,27 @@ bool isNice(inout(char[]) line) {
 }
 
 unittest {
-	assert(isNice("ugknbfddgicrmopn"));
-	assert(isNice("aaa"));
-	assert(!isNice("jchzalrnumimnmhp"));
-	assert(!isNice("haegwjzuvuyypxyu"));
-	assert(!isNice("dvszwmarrgswjxmb"));
+	assert(isNiceFirst("ugknbfddgicrmopn"));
+	assert(isNiceFirst("aaa"));
+	assert(!isNiceFirst("jchzalrnumimnmhp"));
+	assert(!isNiceFirst("haegwjzuvuyypxyu"));
+	assert(!isNiceFirst("dvszwmarrgswjxmb"));
+}
+
+bool isNiceSecond(inout(char[]) lines) {
+	char[] line = lines.dup;
+	auto pair = ctRegex!(`(..).*?\1`); //`
+	auto repeat = ctRegex!(`(.).\1`); //`
+
+	auto a = line.matchFirst(pair);
+	auto b = line.matchFirst(repeat);
+
+	return !a.empty && !b.empty;
+}
+
+unittest {
+	assert(isNiceSecond("qjhvhtzxzqqjkmpb"));
+	assert(isNiceSecond("xxyxx"));
+	assert(!isNiceSecond("uurcxstgmygtbstg"));
+	assert(!isNiceSecond("ieodomkazucvgmuy"));
 }
