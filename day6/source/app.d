83babfdf05a3f936bb32e99b809a824d65c1d1c5
Dan Printzell <xwildn00bx@gmail.com>
2015-12-10 00:15:59 +0100

    Added day6 part 2, also image outputting
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- day6/source/app.d
+++ day6/source/app.d
@@ -5,43 +5,85 @@ import std.algorithm;
 import std.conv;
 
 int main(string[] args) {
-	bool[1000][1000] lights;
+	assert(args.length == 2, "USAGE: day6 <Part>");
+	ulong part = args[1].to!ulong;
+	ubyte[1000][1000] lights;
 	auto r = ctRegex!(`(?P<type>turn on|toggle|turn off) (?P<d1>\d+,\d+) through (?P<d2>\d+,\d+)`); //`
 
-	void doIt(T)(T line) {
-		char[] type = line["type"];
-		ulong[] d1 = line["d1"].split(",").map!(to!ulong).array;
-		ulong[] d2 = line["d2"].split(",").map!(to!ulong).array;
-
-		if (type == "turn on") {
-			for (ulong x = d1[0]; x <= d2[0]; x++)
-				for (ulong y = d1[1]; y <= d2[1]; y++)
-					lights[x][y] = true;
-		} else if (type == "toggle") {
-			for (ulong x = d1[0]; x <= d2[0]; x++)
-				for (ulong y = d1[1]; y <= d2[1]; y++)
-					lights[x][y] = !lights[x][y];
-		} else if (type == "turn off") {
-			for (ulong x = d1[0]; x <= d2[0]; x++)
-				for (ulong y = d1[1]; y <= d2[1]; y++)
-					lights[x][y] = false;
-		} else
-			assert(0, type);
-	}
-
-	foreach (line; stdin
-		.byLine
-		.map!(line => line.matchFirst(r)))
-		doIt(line);
+	if (part == 1)
+		foreach (line; stdin.byLine.map!(line => line.matchFirst(r)))
+			doItFirst(line, lights);
+	else if (part == 2)
+		foreach (line; stdin.byLine.map!(line => line.matchFirst(r)))
+			doItSecond(line, lights);
 
 	ulong count = 0;
+	ulong amount = 0;
 
 	for (ulong x = 0; x < lights.length; x++)
-		for (ulong y = 0; y < lights[x].length; y++)
-			if (lights[x][y])
+		for (ulong y = 0; y < lights[x].length; y++) {
+			if (lights[x][y] == 255)
 				count++;
+			amount += lights[x][y];
+		}
 
-	count.writeln(" Lights are on!");
+	if (part == 1)
+		writeln(count, " Lights are on!");
+	else if (part == 2)
+		writeln(amount, " Combined!");
+
+	File f = File("lights.pbm", "w");
+	f.writeln("P2");
+	f.writeln(lights.length, " ", lights[0].length);
+	f.writeln("255");
+	for (ulong x = 0; x < lights.length; x++) {
+		for (ulong y = 0; y < lights[x].length; y++)
+			f.write((lights[x][y]), " ");
+		f.writeln();
+	}
+	f.close();
 
 	return 0;
+}
+
+void doItFirst(T)(T line, ref ubyte[1000][1000] lights) {
+	char[] type = line["type"];
+	ulong[] d1 = line["d1"].split(",").map!(to!ulong).array;
+	ulong[] d2 = line["d2"].split(",").map!(to!ulong).array;
+
+	if (type == "turn on") {
+		for (ulong x = d1[0]; x <= d2[0]; x++)
+			for (ulong y = d1[1]; y <= d2[1]; y++)
+				lights[x][y] = 255;
+	} else if (type == "toggle") {
+		for (ulong x = d1[0]; x <= d2[0]; x++)
+			for (ulong y = d1[1]; y <= d2[1]; y++)
+				lights[x][y] = (lights[x][y] == 255) ? 0 : 255;
+	} else if (type == "turn off") {
+		for (ulong x = d1[0]; x <= d2[0]; x++)
+			for (ulong y = d1[1]; y <= d2[1]; y++)
+				lights[x][y] = 0;
+	} else
+		assert(0, type);
+}
+
+void doItSecond(T)(T line, ref ubyte[1000][1000] lights) {
+	char[] type = line["type"];
+	ulong[] d1 = line["d1"].split(",").map!(to!ulong).array;
+	ulong[] d2 = line["d2"].split(",").map!(to!ulong).array;
+
+	if (type == "turn on") {
+		for (ulong x = d1[0]; x <= d2[0]; x++)
+			for (ulong y = d1[1]; y <= d2[1]; y++)
+				lights[x][y]++;
+	} else if (type == "toggle") {
+		for (ulong x = d1[0]; x <= d2[0]; x++)
+			for (ulong y = d1[1]; y <= d2[1]; y++)
+				lights[x][y] += 2;
+	} else if (type == "turn off") {
+		for (ulong x = d1[0]; x <= d2[0]; x++)
+			for (ulong y = d1[1]; y <= d2[1]; y++)
+				lights[x][y] = cast(ubyte)max(0, lights[x][y] - 1);
+	} else
+		assert(0, type);
 }
